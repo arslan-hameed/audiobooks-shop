1303af44504c5cbb97f3811550d3101dc474cedd
Arslan Hameed <arslanh@Arslans-MacBook-Pro-2.local>
2025-04-01 20:11:45 +0500

    all files v1

---
+++ script.js
@@ -0,0 +1,41 @@
+// subscribeForm
+document
+  .getElementById("subscribeForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const email = document.getElementById("email").value;
+    const formMessage = document.getElementById("formMessage");
+
+    if (email.includes("@") && email.includes(".")) {
+      formMessage.textContent = "Thank you for subscribing!";
+      formMessage.style.color = "green";
+    } else {
+      formMessage.textContent = "Please enter a valid email address.";
+      formMessage.style.color = "red";
+    }
+  });
+// subscribeForm
+
+document
+  .getElementById("registrationForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const company = document.getElementById("company").value.trim();
+    const website = document.getElementById("website").value.trim();
+    const formMessage = document.getElementById("formMessage");
+
+    if (name === "" || email === "" || company === "" || website === "") {
+      formMessage.textContent = "All fields are required!";
+      formMessage.style.color = "red";
+      return;
+    }
+
+    formMessage.textContent = "Registration successful!";
+    formMessage.style.color = "green";
+
+    // Clear form fields
+    document.getElementById("registrationForm").reset();
+  });
