1026bf127be844f5e0caf6d6199560844a00930a
arslan-hameed <arslan.hameed13@gmail.com>
2025-04-02 20:15:57 +0500

    script location change

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const nav = document.querySelector("nav");
+
+  menuToggle.addEventListener("click", function () {
+      nav.classList.toggle("active");
+  });
+});
 // subscribeForm
 document
   .getElementById("subscribeForm")
